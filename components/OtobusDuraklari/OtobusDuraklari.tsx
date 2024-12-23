280ad511217e355f1ae3c2fd7f926ab96bde7f0e
İnanç Akduvan <49209002+inancakduvan@users.noreply.github.com>
2024-12-23 12:49:08 +0300

    eslint configs updated

--- components/OtobusDuraklari/OtobusDuraklari.tsx
+++ components/OtobusDuraklari/OtobusDuraklari.tsx
@@ -34,11 +34,11 @@ const OtobusDuraklari = () => {
                 setBusDirectionEnd(bus.HAT_BITIS);
             }
 
-            // @ts-expect-error
+            // @ts-expect-error response is unknown
             const duraklar1 = dataDuraklar1[busNo];
             setStations1(duraklar1);
 
-            // @ts-expect-error
+            // @ts-expect-error response is unknown
             const duraklar2 = dataDuraklar2[busNo];
             setStations2(duraklar2);
 
