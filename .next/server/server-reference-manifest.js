9abe774092f8e1e706c5afb37b13aad89055c6de
İnanç Akduvan <49209002+inancakduvan@users.noreply.github.com>
2025-01-13 21:48:46 +0300

    styles added

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HvqyBMK29IOXC33C218rd47RAusLUOJJuIHam7lKO6o=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
