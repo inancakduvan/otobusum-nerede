9962d10de8fdbad4e799d3335a999ffa661db76d
İnanç Akduvan <49209002+inancakduvan@users.noreply.github.com>
2025-01-15 12:07:56 +0300

    unnecessary tags removed

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import BusLines from "@/components/bus-lines";
 import BusLinesContextProvider from "@/context/bus-lines";
 
 export default function Home() {
-  return (<>
-      <BusLinesContextProvider>
-        <BusLines />
-      </BusLinesContextProvider>
-  </>)
+  return (
+    <BusLinesContextProvider>
+      <BusLines />
+    </BusLinesContextProvider>
+  )
 }
